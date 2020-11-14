d2f627b355504ad0e60d73b1343163682e4076a2
edwardlyduong <edward.ly.duong@gmail.com>
2020-11-14 00:27:49 -0800

    added the yintercept code

--- SlopeFormula.java
+++ SlopeFormula.java
@@ -6,26 +6,16 @@ public class SlopeFormula {
     {
         return (y2 - y1) / (x2 - x1);
     }
-    public static void main (String [] args){
-    double x1 = 67;
-    double y1 = 27.3;
-    double x2 = 426; 
-    double y2 = 92;
-    System.out.println("Slope is " + slope(x1, y1, x2, y2));
-
-
-
-
-
+    static double yintercept(double x1, double y1, double x2, double y2)
+    {
+        return y1 - (slope(x1, y1, x2, y2) * x1);
     }
-        
-
-
-
-
-
-
-
-
-
+    public static void main (String [] args){
+    double x1 = 1;
+    double y1 = 1;
+    double x2 = 4; 
+    double y2 = 4;
+    System.out.println("Slope is " + slope(x1, y1, x2, y2));
+    System.out.println("Slope intercept formula is " + "y = " + slope(x1, y1, x2, y2) + "x " + "+ " + yintercept(x1, y1, x2, y2));
+    }
 }
