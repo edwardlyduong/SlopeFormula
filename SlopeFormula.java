58fa6e19d2ad97ced6e26cc50e11357b11051d98
edwardlyduong <edward.ly.duong@gmail.com>
2020-11-15 02:13:40 -0800

    added do while

--- SlopeFormula.java
+++ SlopeFormula.java
@@ -12,33 +12,36 @@ public class SlopeFormula {
         return y1 - (slope(x1, y1, x2, y2) * x1);
     }
     public static void main (String [] args){
+
     Scanner input = new Scanner(System.in);
 
+    String conformation;
+    double x1;
+    double y1;
+    double x2;
+    double y2;
+
+    do{
+
     System.out.println("Enter your x1");
-    double x1 = input.nextDouble();
+    x1 = input.nextDouble();
     
     System.out.println("Enter your y1");
-    double y1 = input.nextDouble();
+    y1 = input.nextDouble();
 
     System.out.println("Enter your x2");
-    double x2 = input.nextDouble();
+    x2 = input.nextDouble();
 
     System.out.println("Enter your y2");
-    double y2 = input.nextDouble();
+    y2 = input.nextDouble();
 
     System.out.println("Are these your two coordinates" + "(" + x1 + " , " + y1 + ")" + " " + "(" + x2 + " , " + y2 + ")");
     System.out.println("Say 'yes' if they are right say 'no' if they aren't.");
-    String conformation = input.next();
-    if(conformation == "yes"){
-    
-    } 
-    else if(conformation == "no"){
-
+    conformation = input.next();
     }
-    while(conformation == "no"){
     
-    }
-
+    while(conformation.equals("no"));
+    
 
     System.out.println("Slope is " + slope(x1, y1, x2, y2));
     System.out.println("Slope intercept formula is " + "y = " + slope(x1, y1, x2, y2) + "x " + "+ " + yintercept(x1, y1, x2, y2));
